00f5f3022c016f47bef3b969a92a773268fb787d
Prince Narteh <prinarteh@gmail.com>
2024-07-03 17:27:26 +0000

    minor changes

--- actions/courses.action.ts
+++ actions/courses.action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { db } from "@/lib/db";
 import { createCourseSchema } from "@/utils/validationSchemas";
 import { auth } from "@clerk/nextjs/server";
 import { z } from "zod";
@@ -8,10 +9,17 @@ export async function createCourse(
   formData: z.infer<typeof createCourseSchema>,
 ) {
   const { userId } = auth();
-  if (!userId) return {};
+  if (!userId) return { message: "Unauthorized" };
   const res = createCourseSchema.safeParse(formData);
+  if (res.error) {
+    return {
+      message: "Error creating course",
+      errors: res.error.flatten().fieldErrors,
+    };
+  }
 
   if (res.success) {
-    return res.data;
+    await db.course.create({ data: { ...res.data } });
+    return { message: "Course created successfully" };
   }
 }
