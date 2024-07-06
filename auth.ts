d555be776634a0a7431e31f0238e367b8322d06f
Prince Narteh <prinarteh@gmail.com>
2024-07-06 19:35:59 +0000

    feat: add user model

--- auth.ts
+++ auth.ts
@@ -1,5 +1,22 @@
 import NextAuth from "next-auth";
+import Credentials, { CredentialInput } from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials;
+      },
+    }),
+  ],
 });
